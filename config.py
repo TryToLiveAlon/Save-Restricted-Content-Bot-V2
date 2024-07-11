6121b3ca2b7b61c205479f7998f93760651ce6cb
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-07-11 22:48:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "20970411"))
+API_HASH = getenv("API_HASH", "6f059b1001f776a1622a1688f0e637cb")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", "6140468904"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002209650474"))
+FORCESUB = getenv("FORCESUB", "deathking_worldd")
