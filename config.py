200663427c058a5b61f4f375b8bfa31964a0ce41
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-07-12 00:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "20970411"))
 API_HASH = getenv("API_HASH", "6f059b1001f776a1622a1688f0e637cb")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7434585617:AAGJJmkz_qUrD1wC_qmwqLfEN57c62rRjQw")
 OWNER_ID = int(getenv("OWNER_ID", "6140468904"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://helllisalivw:bgdfjydsbjisscb@cluster0.lqwa68x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002209650474"))
 FORCESUB = getenv("FORCESUB", "deathking_worldd")
