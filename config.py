945de42df6a188fe9f8bab816348054d6f14aed2
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-03-03 00:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "29385418"))
+API_HASH = getenv("API_HASH", "5737577bcb32ea1aac1ac394b96c4b10")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6660736046").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://towaye6074:A2jR9c7DCr3gX09q@cluster0.g8c3j.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP = getenv("LOG_GROUP", "-1002209650474")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002010103322"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "10"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+WEBSITE_URL = getenv("WEBSITE_URL", "Modijiurl.com")
+AD_API = getenv("AD_API", "cecd5d2f94043ddd1a36edc074e4eb18b13b39f1")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
