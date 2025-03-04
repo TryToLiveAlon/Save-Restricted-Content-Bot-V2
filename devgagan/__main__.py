18c371f8816ec11bedeb26770d8a1b94c32f2995
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-03-04 11:18:41 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -42,8 +42,8 @@ async def devggn_boot():
                 and uploading them back to Telegram.
 👨‍💻 Author: Gagan
 🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+📬 Telegram: https://t.me/Trytolivealon
+▶️ YouTube: https://youtube.com
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
