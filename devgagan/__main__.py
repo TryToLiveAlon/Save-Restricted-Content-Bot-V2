60ad65800545808b3610764c23462ce22a4cfabb
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2026-01-04 21:38:05 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -40,12 +40,10 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
-🗓️ Created: 2025-01-11
-🔄 Last Modified: 2025-01-11
+👨‍💻 Author: Death
+📬 Telegram: https://t.me/Deathkingworld
+🗓️ Created: 2026-01-11
+🔄 Last Modified: 2026-01-11
 🛠️ Version: 2.0.5
 📜 License: MIT License
 ---------------------------------------------------
